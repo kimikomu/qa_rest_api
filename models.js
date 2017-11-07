8f7ae613c907c363d95118426d921165efb861a3
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-11-06 19:22:20 -0500

    Add schema hook and logic

--- models.js
+++ models.js
@@ -3,6 +3,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const sortAnswers = function(a, b) {
+    if(a.votes === b.votes) {
+        return b.updatedAt - a.updatedAt;
+    }
+    return b.votes - a.votes;
+}
+
 const AnswerSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
@@ -16,6 +23,26 @@ const QuestionSchema = new Schema({
     answers: [AnswerSchema]
 });
 
-const Question  = mongoose.model('Questions', QuestionSchema);
+AnswerSchema.method('update', function(updates, callback) {
+    Object.assign(this, updates, {updatedAt: new Date()});
+    this.parent().save(callback);
+});
+
+AnswerSchema.method('vote', function(votes, callback) {
+    if(vote === 'up') {
+        this.votes += 1;
+    } else {
+        this.votes -= 1;
+    }
+    this.parent().save(callback);    
+});
+
+// add a hook to keep answer votes sorted
+QuestionSchema.pre('save', function(next) {
+    this.answers.sort(sortAnswers);
+    next();
+});
+
+const Question = mongoose.model('Questions', QuestionSchema);
 
 module.exports.Question = Question;
